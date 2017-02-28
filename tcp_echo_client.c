129956c9730b359f526c9a302de3fdcb338b8a53
combu <combu@inl.ics.keio.ac.jp>
2017-03-01 00:28:44 +0900

    rm a.out

---
+++ tcp_echo_client.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define ECHOMAX 32
+
+struct msg_echo {
+	unsigned short seq;
+	unsigned short reserve;
+	char msg[32];
+};
+
+int main(int argc, char* argv[])
+{
+	int sock;
+	struct sockaddr_in echoServAddr;
+	struct sockaddr_in fromAddr;
+	unsigned short echoServerPort;
+	socklen_t fromSize;
+	char *servIP;
+	int echoMsgLen;
+	int respMsgLen;
+	struct msg_echo message;
+
+	if (argc != 3) {
+		fprintf(stderr, "Usage: %s <Server IP><Server port>\n", argv[0]);
+		exit(1);
+	}
+
+	servIP = argv[1];
+	
+	if (argc == 3) {
+		echoServerPort = atoi(argv[2]);
+	} else {
+		echoServerPort = 7;
+	}
+
+
+	//create a socket
+	if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+		fprintf(stderr, "sock failed\n");
+		exit(2);
+	}
+	//initialize
+	memset(&echoServAddr, 0, sizeof(echoServAddr));
+	echoServAddr.sin_family = AF_INET;
+	echoServAddr.sin_addr.s_addr = inet_addr(servIP);
+	echoServAddr.sin_port = htons(echoServerPort);
+
+	message.seq = 0;
+	while (1) {
+		fprintf(stdout, "message: ");
+		fgets(message.msg, 32, stdin);
+		if (sendto(sock, &message, sizeof(struct msg_echo), 0, 
+		(struct sockaddr *)&echoServAddr, sizeof(echoServAddr)) < 0) {
+			fprintf(stderr, "sendto failed\n");
+			exit(4);
+		}
+	
+		fromSize = sizeof(fromAddr);
+		if ((respMsgLen = recvfrom(sock, &message, sizeof(struct msg_echo), 0,
+				(struct sockaddr *)&echoServAddr, &fromSize)) < 0 ) {
+		   	fprintf(stderr, "recv failed\n");
+			exit(5);
+		}
+		fprintf(stdout, "received message = %s", message.msg);
+		fprintf(stdout, "seq = %d\n", message.seq);
+		if (message.seq > 9) {
+			break;
+		}
+	}
+	close(sock);
+	exit(0);	
+}
---
+++ tcp_echo_server.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define ECHOMAX 32
+
+struct echo_msg {
+	unsigned short seq;
+	unsigned short reserve;
+	char msg[32];
+};
+
+int main(int argc, char* argv[])
+{
+	int sock;
+	struct sockaddr_in echoServAddr;
+	struct sockaddr_in echoClntAddr;
+	unsigned int cliAddrLen;
+	char echoBuffer[ECHOMAX];
+	unsigned short echoServPort;
+	int recvMsgSize;
+	struct echo_msg message;
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <UDP SERVER PORT>\n", argv[0]);
+		exit(1);
+	}
+	//get server port number
+	echoServPort = atoi(argv[1]);
+	
+	while(1) {
+		//create a socket
+		if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+			fprintf(stderr, "sock failed\n");
+			exit(2);
+		}
+		//initialize
+		memset(&echoServAddr, 0, sizeof(echoServAddr));
+		echoServAddr.sin_family = AF_INET;
+		echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+		echoServAddr.sin_port = htons(echoServPort);
+	
+		//bind server addr to the socket
+		if (bind(sock, (struct sockaddr *) &echoServAddr, 
+					sizeof(echoServAddr)) < 0) {
+			fprintf(stderr, "bind failed");
+			exit(3);
+		}
+
+		//receive and send loop
+		for (;;) {
+		cliAddrLen = sizeof(echoClntAddr);
+
+			if ((recvMsgSize = recvfrom(sock, &message, 
+					sizeof(struct echo_msg),0, 
+					(struct sockaddr *)&echoClntAddr, &cliAddrLen)) < 0) {
+				fprintf(stderr, "recv failed");
+				exit(4);
+			}
+			fprintf(stdout, "Handling client %s\n", 
+					inet_ntoa(echoClntAddr.sin_addr));
+			fprintf(stdout, "message = %s\n", message.msg);
+			message.seq++;
+			if (sendto(sock, &message, sizeof(struct echo_msg), 0,
+					(struct sockaddr *) &echoClntAddr, 
+					sizeof(echoClntAddr)) !=recvMsgSize) {
+				fprintf(stderr, "sendto() failed");
+				exit(5);
+			}
+		}
+	}
+}
