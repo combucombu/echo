2f0a479547a2350ed7966eb1150da5d19ceea3b4
combu <combu@inl.ics.keio.ac.jp>
2017-03-01 00:21:52 +0900

    udp finish when seq over 10

--- udp_echo_server.c
+++ udp_echo_server.c
@@ -32,40 +32,45 @@ int main(int argc, char* argv[])
 	//get server port number
 	echoServPort = atoi(argv[1]);
 	
-	//create a socket
-	if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
-		fprintf(stderr, "sock failed\n");
-		exit(2);
-	}
-	//initialize
-	memset(&echoServAddr, 0, sizeof(echoServAddr));
-	echoServAddr.sin_family = AF_INET;
-	echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	echoServAddr.sin_port = htons(echoServPort);
+	while(1) {
+		//create a socket
+		if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+			fprintf(stderr, "sock failed\n");
+			exit(2);
+		}
+		//initialize
+		memset(&echoServAddr, 0, sizeof(echoServAddr));
+		echoServAddr.sin_family = AF_INET;
+		echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+		echoServAddr.sin_port = htons(echoServPort);
 	
-	//bind server addr to the socket
-	if (bind(sock, (struct sockaddr *) &echoServAddr, 
-				sizeof(echoServAddr)) < 0) {
-		fprintf(stderr, "bind failed");
-		exit(3);
-	}
+		//bind server addr to the socket
+		if (bind(sock, (struct sockaddr *) &echoServAddr, 
+					sizeof(echoServAddr)) < 0) {
+			fprintf(stderr, "bind failed");
+			exit(3);
+		}
 
-	//receive and send loop
-	for (;;) {
+		//receive and send loop
+		for (;;) {
 		cliAddrLen = sizeof(echoClntAddr);
 
-		if ((recvMsgSize = recvfrom(sock, &message, sizeof(struct echo_msg),
-				0, (struct sockaddr *)&echoClntAddr, &cliAddrLen)) < 0) {
-			fprintf(stderr, "recv failed");
-			exit(4);
-		}
-		fprintf(stdout, "Handling client %s\n", 
-				inet_ntoa(echoClntAddr.sin_addr));
-		if (sendto(sock, &message, sizeof(struct echo_msg), 0,
-				(struct sockaddr *) &echoClntAddr, sizeof(echoClntAddr)) !=
-				recvMsgSize) {
-			fprintf(stderr, "sendto() failed");
-			exit(5);
+			if ((recvMsgSize = recvfrom(sock, &message, 
+					sizeof(struct echo_msg),0, 
+					(struct sockaddr *)&echoClntAddr, &cliAddrLen)) < 0) {
+				fprintf(stderr, "recv failed");
+				exit(4);
+			}
+			fprintf(stdout, "Handling client %s\n", 
+					inet_ntoa(echoClntAddr.sin_addr));
+			fprintf(stdout, "message = %s\n", message.msg);
+			message.seq++;
+			if (sendto(sock, &message, sizeof(struct echo_msg), 0,
+					(struct sockaddr *) &echoClntAddr, 
+					sizeof(echoClntAddr)) !=recvMsgSize) {
+				fprintf(stderr, "sendto() failed");
+				exit(5);
+			}
 		}
 	}
 }
